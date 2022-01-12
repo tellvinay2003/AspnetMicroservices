c9fd284b282b1d2f7addbf1aec7c80b23a50aeef
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2022-01-12 12:26:36 +0530

    in sync

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,37 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        
+        private ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName: {request.ProductName} is not found"));
+            }
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+
+            return couponModel;
+        }
+    }
+}
--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,7 @@ namespace Discount.Grpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +34,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<DiscountService>();
 
                 endpoints.MapGet("/", async context =>
                 {
